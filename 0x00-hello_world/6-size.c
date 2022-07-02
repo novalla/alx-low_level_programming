911fb9a958e89b765f24e75fa44cfe06b63b4897
novalla <novalla.derek@gmail.com>
2022-07-01 19:25:48 -0700

    another shell script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@
  * Return:Always 0 (Sucess)
  */
 
-int mai(void)
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
